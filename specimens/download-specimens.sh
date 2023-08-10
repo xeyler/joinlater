098a1543ca95087c8aabc7d5f01c825328d94b05
Brigham Campbell <me@brighamcampbell.com>
2023-08-09 18:28:54 -0600

    Make specimens script download cloudconfig file

--- specimens/download-specimens.sh
+++ specimens/download-specimens.sh
@@ -2,12 +2,14 @@
 
 while read -r link
 do
-	dir=$(echo "$link" | cut -d/ -f2- | cut -d/ -f2- | cut -d/ -f2-)
+	dir=$(echo "$link" | cut -d/ -f4-)
 	echo $dir
 	mkdir -p $dir
 	
-	script_link=$link"php/deploy.php"
-	image_link=$link"images/logo.png"
+	script_link="${link}php/deploy.php"
+	image_link="${link}images/logo.png"
+	cloud_config_link="${link}linux/SecureW2.cloudconfig"
 	wget --content-disposition -P "$dir" --post-data="request=deployLinux" "$script_link"
 	wget -P "$dir" "$image_link"
+	wget -P "$dir" "$cloud_config_link"
 done < "$1"
